73f0947fdda14a8edd38ba93dc8f3c942f48212f
FreadXX <289466360@qq.com>
2016-04-15 21:15:20 +0800

    No Test Version

---
+++ LinearList/DoubleLinkedLinearList.java
@@ -0,0 +1,116 @@
+package com.lsmaker.LinearList;
+
+import com.lsmaker.Node.DoubleNode;
+
+public class DoubleLinkedLinearList<E> extends LinearList<E>{
+	
+	public DoubleLinkedLinearList() {
+		root = null;
+		length = 0;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		
+		return length==0;
+	}
+
+	@Override
+	public void insert(E element) {
+		insert(element, length);
+	}
+
+	@Override
+	public void insert(E element, int index) {
+		if(index < 0 || index > length) {
+			System.out.println("Insert Index Error!");
+			return ;
+		}
+		DoubleNode<E> n = root;
+		DoubleNode<E> newNode = new DoubleNode<E>(element, null, null);
+		if(length == 0) {			// if the root is null, set the new node as the root
+			root = newNode;
+		} else {
+			for(int i=0;i<index;i++) {
+				n = n.getNextNode();
+			}
+			DoubleNode<E> p = n.getNextNode();	
+			if(p != null) {
+				newNode.setNextNode(p);
+				p.setPreviousNode(newNode);
+			}
+			n.setNextNode(newNode);
+			newNode.setPreviousNode(n);
+		}
+		length++;
+	}
+
+	@Override
+	public E delete() {
+		return delete(length-1);
+	}
+
+	@Override
+	public E delete(int index) {
+		DoubleNode<E> result;
+		if(index < 0 || index >= length) {
+			System.out.println("Delete Index Error!");
+			return null;
+		}
+		if(isEmpty() == true) {
+			System.out.println("List Empty! Delete Failed!");
+			return null;
+		}
+		if(length == 1) {
+			result = null;
+
+		} else {
+			DoubleNode<E> n = root;
+			for(int i=0;i<index;i++) {
+				n = n.getNextNode();
+			}
+			result = n.getNextNode();
+			DoubleNode<E> p = result.getNextNode();
+			if(p!=null) {
+				p.setPreviousNode(n);
+			}
+			n.setNextNode(p);
+		}
+		length --;
+		return result.getElement();
+	}
+
+	@Override
+	public int find(E element) {
+		DoubleNode<E> n = root;
+		int index = 0;
+		while (n != null) {
+			if (n.getElement().equals(element)) {
+				return index;
+			} else {
+				n = n.getNextNode();
+				index++;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public String traverse() {
+		if (length == 0) {
+			return "";
+		}
+		StringBuilder result = new StringBuilder();
+		DoubleNode<E> n = root;
+		for (int i = 0; i < length - 1; i++) {
+			result.append(n.getElement().toString() + ",");
+			n = n.getNextNode();
+		}
+		result.append(n.getElement().toString());
+		return result.toString();
+	}
+	
+	
+	private DoubleNode<E> root;
+	private int length;
+}
